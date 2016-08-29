8b1dbeea1a11e0516582bc1d0a353e33ab9e6bba
wuxingjiang <744561016@qq.com>
2016-08-29 18:06:54 +0800

    new demo

---
+++ webpackP/webpack.config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by star on 2016/8/29.
+ */
+module.exports = {
+    entry: __dirname + '/app/main.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        contentBase: './public',
+        colors: true,
+        port: '9999',
+        inline: true,
+        historyApiFallback: true
+    }
+};
+
